4731a966d11368f04fffdc626ce11333d50c8e07
Sanko Robinson <sanko@cpan.org>
2024-09-25 22:20:19 -0400

    Bleh

--- script/miiii.pl
+++ script/miiii.pl
@@ -16,19 +16,19 @@ use App::mii::Markdown;
 use App::mii::Templates;
 #
 class App::mii v1.0.0 {
-    method log ( $msg, @etc ) { say @etc ? sprintf $msg, @etc : $msg; }
-    use JSON::PP   qw[decode_json];    # not in CORE
-    use Path::Tiny qw[];               # not in CORE
+    use JSON::PP qw[decode_json];    # not in CORE
+    use Path::Tiny qw[];             # not in CORE
     use version    qw[];
     #
     field $path : param : reader //= '.';
+    field $package : param //= ();
     #
     field $config : reader;
     field $meta;
     #
     method abstract( $v //= () ) { $config->{abstract} = $v if defined $v; $config->{abstract}; }
     method version( $v //= () )  { $config->{version} = $v if defined $v; version::parse( 'version', $config->{version} ); }
-    method name ()               { $config->{name} }
+    method name ( $v //= () )    { $config->{name} = $v =~ s[::][-]gr if defined $v; $config->{name} }
     method package ()            { $config->{name} =~ s[-][::]gr }
     method author ()             { $config->{author} }
 
@@ -40,19 +40,34 @@ class App::mii v1.0.0 {
         } @{ $config->{license} };
     }
     #
-    ADJUST {
-        $path   = Path::Tiny::path($path) unless builtin::blessed $path;
-        $config = decode_json $path->child('META.json')->slurp_raw;
-    }
+    method log ( $msg, @etc ) { say @etc ? sprintf $msg, @etc : $msg; }
 
-    # init
-    method init($package) {
-        $path->child($_)->touchpath for qw[t lib script];
+    method package2path($package) {
+        $path->child('lib')->child( ( $package =~ s[::][/]gr ) . '.pm' );
+    }
+    #
+    ADJUST {
+        $path = Path::Tiny::path($path)->absolute unless builtin::blessed $path;
+        my $meta = $path->child('META.json');
+        warn;
+        if ( $meta->exists ) {
+            $config = decode_json $meta->slurp_utf8;
+            warn;
+        }
+        elsif ( defined $package ) {
+            $self->name($package);
+            warn;
+        }
+        else {
+            die '...what are we doing? I need a package name or something.';
+        }
+        warn;
     }
 
     # Pull list of files from git
     method gather_files() {
-        my $msg = Capture::Tiny::capture_stdout { system qw[git ls-files] };
+        my ($msg) = $self->git('ls-files');
+        warn $msg;
         map { Path::Tiny::path($_)->canonpath } split /\R+/, $msg;
     }
 
@@ -93,25 +108,112 @@ class App::mii v1.0.0 {
         $path->child('META.json')->spew_utf8( $json->encode( $self->generate_meta ) );
     }
 
-    method write_cpanmeta() {
-        my $cpanfile = Module::CPANfile->load;
-        $cpanfile->merge_meta( $path->child('META.json'), '2.0' );
-        use Data::Dump;
-        ddx $cpanfile;
-        warn $cpanfile->to_string(1);
-        $cpanfile->save('cpanfile');
+    method write_cpanfile() {
+        my $cpanfile = Module::CPANfile->from_prereqs( $self->generate_meta->{prereqs} );
+        $cpanfile->save( $path->child('cpanfile') );
     }
 
-    method make_dist(%args) {
+    method write_pm( $package, $version //= $self->version ) {
+        my $file = $self->package2path($package);
+        return if $file->exists;
+        $file->touchpath;
+        $file->spew_utf8( sprintf <<'END', $package, $version ) }
+package %s %s { ; }; 1;
+__END__
+END
+
+    method dist(%args) {
 
         # %args might contain version, etc.
     }
-    method make_test( $verbose //= 0 ) { }
+    method test( $verbose //= 0 ) { }
+
+    method release( $upload //= 1 ) {
+    }
+
+    method init() {
+                warn;
+
+        {
+            $path->child($_)->mkdir for qw[t lib script eg share];
+        warn;
+
+            # TODO: Create lib/.../....pm
+            $self->write_pm( $self->package );
+                    warn;
+
+            $self->write_meta;
+                    warn;
+
+            $self->write_cpanfile;
+        warn;
+
+            # TODO: .tidyallrc
+            #
+                    warn;
+
+            warn $self->package2path( $self->package );    #->touch;
+        }
+        warn;
+
+        # TODO: create t/000_compile.t
+        $self->git( 'init', $path );
+                warn;
+
+        $self->git( 'add',  $_ ) for qw[cpanfile META.json];
+                warn;
+
+        for my $dir ( map { $path->child($_) } qw[t lib script eg share] ) {
+                    warn;
+
+            $dir->touchpath;
+                    warn;
+
+            $self->git( 'add', $dir );
+                    warn;
+
+        }
+                warn;
+
+    }
+
+    method git(@args) {
+warn;
+        #~ warn join ' ', '>> git', @args;
+        my ( $stdout, $stderr, $exit ) = Capture::Tiny::capture {
+            system 'git', @args;
+        };
+
+        #~ use Data::Dump;
+        #~ ddx [ $stdout, $stderr, $exit ];
+        #~ $stdout;
+    }
+
+        method whoami() {
+        my $me = Capture::Tiny::capture_stdout { system qw[git config user.name] };
+        my $at = Capture::Tiny::capture_stdout { system qw[git config user.email] };
+        $me // return ();
+        chomp $me;
+        chomp $at if $at;
+        $me . ( $at ? qq[ <$at>] : '' );
+    }
 };
 #
 use Data::Dump;
-my $mii = App::mii->new();
-ddx $mii->gather_files;
+$|++;
 
+#~ chdir
+my $mii = App::mii->new(
+    package => 'Net::BitTorrent',
+
+    #~ path => 'Acme-Mii/'
+);
+warn;
+$mii->init();
+warn;
+
+#~ warn $mii->package2path('Acme::Mii');
+#~ ddx $mii->gather_files;
 #~ ddx $mii->generate_meta;
-$mii->write_meta;
+ddx $mii->gather_files;
+warn;
